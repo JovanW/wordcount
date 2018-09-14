23933ba0bb6114cbdc32a4923cd28ce880f228e4
韦智锋 <2311109128@qq.com>
2018-09-14 13:21:58 +0800

    words
    words更改

--- wordcount/wc.cpp
+++ wordcount/wc.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 int c = 0;
 int w = 0;
 int l = 0;
-
-//    fclose(fp);
-
 int main(int argc, char *argv[])
 {
     FILE *fp;
@@ -16,7 +14,7 @@ int main(int argc, char *argv[])
     if( (fp = fopen(file,"r") ) == NULL)		//打开文件并且读取
     {
         printf( "open file failed!\n" );
-        exit(-1);								//
+        exit(-1);
     }
     else if( argc <= 2)
 		printf( "wrong command!" );
@@ -36,19 +34,66 @@ int main(int argc, char *argv[])
 			        printf( "the number of characters:%d\n", c);
 			        break;
 				}
-			case 'w':
-				{
-					while( !feof(fp) )							//读取文件直到左后一个字符
-	    			{
-	    				char a = fgetc(fp);						//大循环来返回每读取的一个字节
-	    			    if( a==' ' || a=='\n' || a=='\t' || a==',' || a=='.' || a=='!' || a=='=' || a==';')
-            				w++;								//单词数加一
-            		}
+			//case 'w':                   //另一种思路：寻找单词头
+/*				{
+                    char a = fgetc(fp);		            //大循环来返回每读取的一个字节
+                    if( isalnum(a) )                    //开头是特殊字符
+                        w--;
+					else
+                    {
+                        while( !feof(fp) )			    //读取文件直到左后一个字符
+                        {
+                            char b = fgetc(fp)
+                            if( isalnum(b) )            //特殊字符，单词数+1
+                                w++;
+                            char c =fgetc(fp);          //
+                            if( !isalnum(b) )
+                            {
+                                if( isalnum(c) && c != EOF)
+                                    w--;
+                            }
+                        }
+                    }
             		w++;
+
             		printf( "the number of words:%d\n", w);
             		break;
-            	}
-			case 'l':
+            	}   */
+            case 'w':
+                {
+                    char a =fgetc(fp);
+                    while( a != EOF)
+                    {
+                        if( (a >='a' && a <='z') || ( a >='A' && a <='Z') || ( a >='0' && a <='9') )
+                        {
+                            while( (a >='a' && a <='z') || ( a >='A' && a <='Z') || ( a >='0' && a <='9') )
+                                a =fgetc(fp);
+                            w++;
+                            a =fgetc(fp);
+                        }
+                        else
+                            a =fgetc(fp);
+                    }
+                    printf("the number of words:%d\n",w);
+                    break;
+                }
+                    //else                            //以特殊符号开头
+                    //if( isalnum(head) ){
+                    /*    while( !feof(fp))
+                        {
+                            char a = fgetc(fp);
+                            if( isalnum(a) )           //返回0
+                                {
+                                    char b =fgetc(fp);
+                                    if( !isalnum(b) )
+                                        w++;
+                                }
+                        }
+                        w++;
+                        printf( "the number of words:%d\n", w);
+                    break;
+                    }   */
+                case 'l':
 				{
 					while( !feof(fp) )							//读取文件直到左后一个字符
 				    {
